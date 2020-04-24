95f1fee879d1d92c8e8eb49dadc9635896a7e0e8
Junlapong L <junlapong@gmail.com>
2020-04-24 10:28:37 +0700

    test sqlite

--- api.php
+++ api.php
@@ -10784,12 +10784,25 @@ namespace Tqdev\PhpCrudApi {
     use Tqdev\PhpCrudApi\ResponseUtils;
 
     $config = new Config([
-        'username' => 'php-crud-api',
-        'password' => 'php-crud-api',
-        'database' => 'php-crud-api'
+        'driver'   => 'sqlite',
+        'address'  => './db/crud.sqlite',
+        // 'port'     => 3306,
+        'database' => './db/crud.sqlite',
+        'username' => 'crud',
+        'password' => 'password'
     ]);
     $request = RequestFactory::fromGlobals();
     $api = new Api($config);
     $response = $api->handle($request);
     ResponseUtils::output($response);
+
+    /*
+
+    sqlite3 /tmp/db-crud.sqlite
+
+    CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);
+    INSERT INTO users(name) VALUES ('Jman');
+    SELECT * FROM users;
+
+    */
 }
--- src/index.php
+++ src/index.php
@@ -10,11 +10,24 @@ use Tqdev\PhpCrudApi\ResponseUtils;
 require '../vendor/autoload.php';
 
 $config = new Config([
-    'username' => 'php-crud-api',
-    'password' => 'php-crud-api',
-    'database' => 'php-crud-api'
+    'driver'   => 'sqlite',
+    'address'  => './db/crud.sqlite',
+    // 'port'     => 3306,
+    'database' => './db/crud.sqlite',
+    'username' => 'crud',
+    'password' => 'password'
 ]);
 $request = RequestFactory::fromGlobals();
 $api = new Api($config);
 $response = $api->handle($request);
 ResponseUtils::output($response);
+
+/*
+
+sqlite3 /tmp/db-crud.sqlite
+
+CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);
+INSERT INTO users(name) VALUES ('Jman');
+SELECT * FROM users;
+
+*/
